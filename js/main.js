0c16ab96a98fa6c7a1b7a4b41b9fad4686a2472b
Skaidrius <skaidrius@gmail.com>
2015-02-25 14:59:54 +0000

    preparing individual post page

--- js/main.js
+++ js/main.js
@@ -218,7 +218,7 @@ app.controller('PopularController', function ($scope) {
 app.controller('PostsController', function ($scope) {
     'use strict';
     $scope.message = "Įrašas";
-    $scope.post = ""; // reikia pasinagrineti sita: http://plnkr.co/edit/gmtcE2?p=preview
+    $scope.post = "";  // reikia pasinagrineti sita: http://plnkr.co/edit/gmtcE2?p=preview
 });
 
 app.controller('AuthorsController', function ($scope) {
